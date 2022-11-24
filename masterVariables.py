6c8fb1dd7d633232501004471492fb0983cb797d
Serpent03 <66001658+Serpent03@users.noreply.github.com>
2022-11-24 16:36:38 +0530

    Create masterVariables.py

---
+++ masterVariables.py
@@ -0,0 +1,27 @@
+apiKey = 'lwOywjHQ6ksVHjCWtgWo8c9rdTutyt7m3fRkZsiYBL6KkNWfnfD8w2vJOdnpavb8'
+secretKey = 'EPiaTWJ5cYrl9i8tEtXrayGcCNy38ymKc4dlh8sGKPPDYIGv0j9fklkw0BpGU49b'
+currency = "ETH"
+quoteCurrency = "USDT"
+tP = f"{currency}{quoteCurrency}"
+
+time = 'time'
+liveClose = 'live-close'
+liveOpen = 'live-open'
+predictedLiveClose = 'p-live-close'
+exponentialAverageShort = 'live-e-average-s'
+exponentialAverageMedium = 'live-e-average-m'
+exponentialAverageLong = 'live-e-average-l'
+
+signal = 'signal'
+sColor = 's-color'
+sMarker = 's-marker'
+sAlpha = 's-alpha'
+
+wallet = 'wb'
+
+ADXIndex = 'm-index'
+rsi = 'rsi'
+
+user = 'dumbpilot03@gmail.com'
+password = 'sgatwdddkrmmhqok'
+imapUrl = 'imap.gmail.com'
